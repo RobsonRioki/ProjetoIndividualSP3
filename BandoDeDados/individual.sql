04a40492f8ab2d64938098563fae39b65961e178
RobsonRioki <robson.nakama@sptech.school>
2023-11-21 11:01:41 -0300

    Adicionando placeholders no login e cadastro, e adicionando a pagina principal do site

--- BandoDeDados/individual.sql
+++ BandoDeDados/individual.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE individual;
-USE individual;
-
+CREATE DATABASE JapanFeed;
+USE JapanFeed;
+drop database japanfeed;
 
 CREATE TABLE usuario(
 idUsuario int primary key auto_increment,
-Nome varchar(70),
+Nome varchar(45),
 email varchar(70),
-senha varchar(45)
+senha varchar(70)
 );
+
