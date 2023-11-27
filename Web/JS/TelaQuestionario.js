999f7882d86cc37897bf4de4069e10532448a461
RobsonRioki <robson.nakama@sptech.school>
2023-11-27 11:09:27 -0300

    arrumando o quiz

--- Web/JS/TelaQuestionario.js
+++ Web/JS/TelaQuestionario.js
@@ -1,5 +1,16 @@
 function registrar() {
-    // alert("okonomiyaki");
-    var sla = document.querySelector('input[name="p1"]:checked').value;
-    alert(sla);
+    var Resposta1 = document.querySelector('input[name="p1"]:checked').value;
+    var Resposta2 = document.querySelector('input[name="p2"]:checked').value;
+    var Resposta3 = document.querySelector('input[name="p3"]:checked').value;
+    
+    
+    alert(Resposta1);
+    alert(Resposta2);
+    alert(Resposta3);
+
+
+
+
+
+
   }
